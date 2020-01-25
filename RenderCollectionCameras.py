8160b0e8e4769b89a0b8515f8f1778971a89f203
joaulo <joaulo.mailbox@gmail.com>
2020-01-25 15:47:39 +0100

    modified render functions to set properly file format and extension to match selection still img/anim

--- RenderCollectionCameras.py
+++ RenderCollectionCameras.py
@@ -1,3 +1,16 @@
+import os
+from bpy.types import (Panel,
+                       Operator,
+                       # Menu,
+                       Scene,
+                       PropertyGroup,
+                       )
+from bpy.props import (StringProperty,
+                       IntProperty,
+                       # CollectionProperty,
+                       PointerProperty,
+                       )
+import bpy
 bl_info = {
     "name": "Render Collection Cameras",
     "description": "Render all the cameras in a selected collection",
@@ -5,25 +18,11 @@ bl_info = {
     "version": (1, 0, 0),
     "blender": (2, 80, 0),
     "location": "3D View > RCCT",
-    "warning": "", # used for warning icon and text in addons panel
+    "warning": "",  # used for warning icon and text in addons panel
     "wiki_url": "",
     "tracker_url": "",
     "category": "Render",
 }
-
-import bpy
-from bpy.props import (StringProperty,
-                       IntProperty,
-                       CollectionProperty,
-                       PointerProperty,
-                       )
-from bpy.types import (Panel,
-                       Operator,
-                       Menu,
-                       Scene,
-                       PropertyGroup,
-                       )
-import os
 
 
 # ------------------------------------------------------------------------
@@ -33,33 +32,33 @@ import os
 class RenderCollectionCamerasProperties(PropertyGroup):
 
     path_dir: StringProperty(
-                            name = "Directory",
-                            description="Choose a directory:",
-                            default="",
-                            maxlen=1024,
-                            subtype='DIR_PATH'
-                            )
-                            
+        name="Directory",
+        description="Choose a directory:",
+        default="",
+        maxlen=1024,
+        subtype='DIR_PATH'
+    )
+
     collection: StringProperty(
-                            name = "Collection",
-                            description="Choose a collection:",
-                            default="",
-                            )
-    
-    start_frame: IntProperty(
-                            name="Start Frame",
-                            description="Start Frame:",
-                            default=1,
-                            min=1
-                            )
-    
-    end_frame: IntProperty(
-                            name="End Frame",
-                            description="End Frame:",
-                            default=100,
-                            min=1
-                            )
-    
+        name="Collection",
+        description="Choose a collection:",
+        default="",
+    )
+
+    # start_frame: IntProperty(
+    #     name="Start Frame",
+    #     description="Start Frame:",
+    #     default=1,
+    #     min=1
+    # )
+
+    # end_frame: IntProperty(
+    #     name="End Frame",
+    #     description="End Frame:",
+    #     default=100,
+    #     min=1
+    # )
+
 
 # ------------------------------------------------------------------------
 #    Operators
@@ -70,18 +69,17 @@ class RenderCollectionCamerasImg(Operator):
     bl_idname = "render.collection_cameras_images"
     bl_label = "Render Collection Cameras Images"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     def execute(self, context):
         scene = context.scene
         rcct = scene.rcct
         print("start rendering cameras in collection:", rcct.collection)
         for cam in [obj for obj in bpy.data.collections[rcct.collection].all_objects if obj.type == 'CAMERA']:
             scene.camera = cam
+            # scene.render.image_settings.engine = 'BLENDER_EEVEE'
+            scene.render.image_settings.file_format = 'JPEG'
+            # scene.render.image_settings.compression = 96
             scene.render.filepath = os.path.join(rcct.path_dir, cam.name)
-#            bpy.ops.anim.change_frame(rcct.start_frame)
-#            bpy.ops.anim.end_frame_set()
-#            bpy.ops.anim.change_frame(rcct.end_frame)
-#            bpy.ops.anim.start_frame_set()
             bpy.ops.render.render(write_still=True)
             scene.render.filepath = rcct.path_dir
 
@@ -93,18 +91,19 @@ class RenderCollectionCamerasAnim(Operator):
     bl_idname = "render.collection_cameras_animations"
     bl_label = "Render Collection Cameras Animations"
     bl_options = {'REGISTER', 'UNDO'}
-    
+
     def execute(self, context):
         scene = context.scene
         rcct = scene.rcct
         print("start rendering cameras in collection:", rcct.collection)
         for cam in [obj for obj in bpy.data.collections[rcct.collection].all_objects if obj.type == 'CAMERA']:
             scene.camera = cam
+            # scene.render.image_settings.engine = 'BLENDER_EEVEE'
+            scene.render.image_settings.file_format = 'FFMPEG'
+            scene.render.ffmpeg.format = 'MPEG4'
+            scene.render.ffmpeg.codec = 'MPEG4'
+
             scene.render.filepath = os.path.join(rcct.path_dir, cam.name)
-#            bpy.ops.anim.change_frame(rcct.start_frame)
-#            bpy.ops.anim.end_frame_set()
-#            bpy.ops.anim.change_frame(rcct.end_frame)
-#            bpy.ops.anim.start_frame_set()
             bpy.ops.render.render(animation=True, write_still=False)
             scene.render.filepath = rcct.path_dir
 
@@ -115,7 +114,7 @@ class RenderCollectionCamerasAnim(Operator):
 #    In-Panel Menus
 # ------------------------------------------------------------------------
 
-#class OBJECT_MT_CustomMenu(Menu):
+# class OBJECT_MT_CustomMenu(Menu):
 #    bl_label = "Custom Render"
 #    bl_idname = "OBJECT_MT_custom_menu"
 
@@ -136,11 +135,10 @@ class RCCT_PT_Panel(Panel):
     bl_label = "Render Collection Cameras"
     bl_idname = "RCCT_PT_Panel"
     bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW" #UI
+    bl_region_type = "WINDOW"  # UI
     bl_category = "RCCT"
     bl_context = "output"
     bl_options = {'DEFAULT_CLOSED'}
-
 
 #    @classmethod
 #    def poll(self,context):
@@ -152,12 +150,12 @@ class RCCT_PT_Panel(Panel):
         rcct = scene.rcct
 
         layout.prop(rcct, "path_dir")
-#        layout.prop(rcct, "start_frame") 
+#        layout.prop(rcct, "start_frame")
 #        layout.prop(rcct, "end_frame")
         layout.prop_search(rcct, "collection", bpy.data, "collections")
         layout.operator("render.collection_cameras_images")
         layout.operator("render.collection_cameras_animations")
-        #layout.menu(OBJECT_MT_CustomMenu.bl_idname, text="Presets", icon="SCENE")
+        # layout.menu(OBJECT_MT_CustomMenu.bl_idname, text="Presets", icon="SCENE")
         layout.separator()
 
 
@@ -169,18 +167,22 @@ classes = (
     RenderCollectionCamerasProperties,
     RenderCollectionCamerasImg,
     RenderCollectionCamerasAnim,
-    #OBJECT_MT_CustomMenu,
+    # OBJECT_MT_CustomMenu,
     RCCT_PT_Panel
 )
 
-def menu_func(self, context):
-    self.layout.operator(RenderCollectionCameras.bl_idname)
+
+# def menu_func(self, context):
+#     self.layout.operator(RenderCollectionCamerasImg.bl_idname)
+#     self.layout.operator(RenderCollectionCamerasAnim.bl_idname)
+
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
     Scene.rcct = PointerProperty(type=RenderCollectionCamerasProperties)
 #    bpy.types.TOPBAR_MT_render.append(menu_func)
+
 
 def unregister():
     for cls in reversed(classes):
